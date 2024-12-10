866c7f46c1b75d784e21368e3781709adb8f02ce
michelangelo64 <markhypov@qualium-systems.com>
2024-12-10 23:27:35 +0200

    [SCRUM-5] feat: connected vuetify UI component library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,32 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/stylelint-module', '@pinia/nuxt'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/stylelint-module',
+    '@pinia/nuxt',
+    (options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', config => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+  ],
   eslint: {},
   stylelint: {},
   css: ['~/styles/main.scss'],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div class="hello">HELLO</div>
+    <v-btn>Button!</v-btn>
   </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <style scoped lang="scss">
 .hello {
